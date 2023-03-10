0b30c6d3b7fa9ea639c60039de4ad6a25ba25059
salimp2009@gmail.com <salimp2009@gmail.com>
2023-03-10 18:25:27 +0300

    added decreaseButtonEl and resetButtonEl; need to add limits for decrease

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const increaseButtonEl = document.querySelector(".counter__button--increase");
+const decreaseButtonEl = document.querySelector(".counter__button--decrease");
+const resetButtonEl = document.querySelector(".counter__reset-button");
 const counterValueEl = document.querySelector(".counter__value");
 
 increaseButtonEl.addEventListener("click", () => {
@@ -6,4 +8,15 @@ increaseButtonEl.addEventListener("click", () => {
   const currentValueAsNumber = +currentValue;
   const newValue = currentValueAsNumber + 1;
   counterValueEl.textContent = newValue;
+});
+
+decreaseButtonEl.addEventListener("click", () => {
+  const currentValue = counterValueEl.textContent;
+  const currentValueAsNumber = +currentValue;
+  const newValue = currentValueAsNumber - 1;
+  counterValueEl.textContent = newValue;
+});
+
+resetButtonEl.addEventListener("click", () => {
+  counterValueEl.textContent = 0;
 });
