d093f5aec94a0f9558b06f099ddb51cea123b434
salimp2009@gmail.com <salimp2009@gmail.com>
2023-03-11 19:28:08 +0300

    added limit for counter to 5 and display message and change color, reset button resets

--- script.js
+++ script.js
@@ -1,20 +1,36 @@
+const counterEl = document.querySelector(".counter");
 const increaseButtonEl = document.querySelector(".counter__button--increase");
 const decreaseButtonEl = document.querySelector(".counter__button--decrease");
 const resetButtonEl = document.querySelector(".counter__reset-button");
 const counterValueEl = document.querySelector(".counter__value");
+const counterTitleEl = document.querySelector(".counter__title");
 
 function incrementCounter() {
   const currentValue = counterValueEl.textContent;
   const currentValueAsNumber = +currentValue;
-  const newValue = currentValueAsNumber + 1;
+  let newValue = currentValueAsNumber + 1;
+  if (newValue > 5) {
+    newValue = 5;
+    // no dot required below to identify class since it is classList
+    counterEl.classList.add("counter--limit");
+    counterTitleEl.innerHTML = "Limited! Buy <b>Pro</b> for >5!";
+    increaseButtonEl.disabled = true;
+    decreaseButtonEl.disabled = true;
+  }
   counterValueEl.textContent = newValue;
+  increaseButtonEl.blur();
 }
 
 function decrementCounter() {
   const currentValue = counterValueEl.textContent;
   const currentValueAsNumber = +currentValue;
-  const newValue = currentValueAsNumber - 1;
+  let newValue = currentValueAsNumber - 1;
+
+  if (newValue < 0) {
+    newValue = 0;
+  }
   counterValueEl.textContent = newValue;
+  decreaseButtonEl.blur();
 }
 
 increaseButtonEl.addEventListener("click", incrementCounter);
@@ -23,6 +39,12 @@ decreaseButtonEl.addEventListener("click", decrementCounter);
 
 resetButtonEl.addEventListener("click", () => {
   counterValueEl.textContent = 0;
+  counterEl.classList.remove("counter--limit");
+  counterTitleEl.textContent = "Fancy Counter";
+  increaseButtonEl.disabled = false;
+  decreaseButtonEl.disabled = false;
+  // to prevent space button press event twice due to focus
+  resetButtonEl.blur();
 });
 
 document.addEventListener("keydown", incrementCounter);
