4004f11ad8ae3ba093a7fec818e5468860fdc887
salimp2009@gmail.com <salimp2009@gmail.com>
2023-03-11 14:44:03 +0300

    added keydow event to increase the counter value; there is a bug; it does not work decrease button or reset is clicked; in progress

--- script.js
+++ script.js
@@ -3,20 +3,26 @@ const decreaseButtonEl = document.querySelector(".counter__button--decrease");
 const resetButtonEl = document.querySelector(".counter__reset-button");
 const counterValueEl = document.querySelector(".counter__value");
 
-increaseButtonEl.addEventListener("click", () => {
+function incrementCounter() {
   const currentValue = counterValueEl.textContent;
   const currentValueAsNumber = +currentValue;
   const newValue = currentValueAsNumber + 1;
   counterValueEl.textContent = newValue;
-});
+}
 
-decreaseButtonEl.addEventListener("click", () => {
+function decrementCounter() {
   const currentValue = counterValueEl.textContent;
   const currentValueAsNumber = +currentValue;
   const newValue = currentValueAsNumber - 1;
   counterValueEl.textContent = newValue;
-});
+}
+
+increaseButtonEl.addEventListener("click", incrementCounter);
+
+decreaseButtonEl.addEventListener("click", decrementCounter);
 
 resetButtonEl.addEventListener("click", () => {
   counterValueEl.textContent = 0;
 });
+
+document.addEventListener("keydown", incrementCounter);
